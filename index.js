bc578ecb7e6b095e7fb3251784f07c5138fb73ef
Owner <ayodipeadeyemi@gmail.com>
2021-06-21 22:41:49 -0500

    updated

---
+++ index.js
@@ -0,0 +1,649 @@
+
+//1.  Single Line Comment
+//Handle click to hand two numbers
+
+// 2.  Multiple Line Comment
+
+/* 
+Multiple Line Comment
+Multiple Line Comment
+Multiple Line Comment
+*/
+//3. Variables and Values
+//4. Let and Const
+
+//5 Data type: 
+   /*a. Strings -----Strings are text
+   b. Number 
+   c. Boolean either of true or false
+   d. Undefined
+
+
+
+   Operator
+
+  let Name  = 'text goes here'
+  1. equal Sign.
+
+*/
+
+// // // 2. Plus sign  
+// // let result = 13 + 1
+// // //3. Minus
+// // result = 14-10
+
+// // //4. Multiple
+
+// // result = 4 * 4
+
+
+// // // 5. Division
+
+
+// // result = 15/3
+
+// // // 6. percenter
+
+// // result = 10 % 3 === 1
+
+// // 123456789
+// // testimony
+
+// //   console.log(result)
+
+// // //variable declaration
+
+
+
+
+// // var today = 'Todady is a sunny dayTodady is a sunny dayTodady is a sunny dayTodady is a sunny dayTodady is a sunny dayTodady is a sunny day'
+// // document.getElementById('page').innerText = today
+
+// // var isVariable  =  's t r u c t u r e'
+
+// // var newNumber = 10
+
+// // var isMarried = false || true
+
+// // var specialNumber;
+
+// // //  Primitive
+
+// // var newArray = [10, 'string', false ]
+// // var newArray2 = ['Adeyemi', 'Taiwo', 'Terry' ]
+
+// // // length = 3
+
+// // // document.getElementById('array').innerText = newArray[0]
+
+// // var newObject  = {
+// //   person : 'Adeyemi',
+// //   age: 29,
+// //   isMarried : true,
+
+// //   printPersonInfo: function(){
+// //     return person + age + isMarried
+// //   }
+// // };
+
+
+// // document.getElementById('object').innerText = newArray
+
+// // console.log(newObject.person)
+// // console.log(newObject.age)
+// // console.log(newObject.isMarried)
+
+
+// // Working with Operator
+
+// //1. Addition ----- +
+// // var x =5;
+// // var y = 10
+
+// // var plusOperator1 = x+ y
+
+// // var plusOperator2 = 10 + 20
+
+// // console.log(plusOperator1)
+// // console.log(plusOperator2)
+// // 2. Minus
+// // var x =5;
+// // var y = 10
+
+// // var minusOperator1 = x  - y
+
+// // var minusOperator2 = 20 - 10
+
+// // console.log(minusOperator1)
+// // console.log(minusOperator2)
+
+
+// // 3. Multiplication
+
+// // var x =5;
+// // var y = 10
+
+// // var multiplyOperator1 = x  * y
+
+// // var multiplyOperator2 = 20 * 10
+
+// // console.log(multiplyOperator1)
+// // console.log(multiplyOperator2)
+
+
+// // 4. Division
+
+// // var x =5;
+// // var y = 10
+
+// // var divisionOperator1 = x  / y
+
+// // var divisionOperator2 = 20 / 10
+
+// // console.log(divisionOperator1)
+// // console.log(divisionOperator2)
+
+// // 5. Perccentile //Remainder
+
+
+// // var x =10;
+// // var y = 3
+
+
+// // // === 'Even'
+// // var remainderOperator1 = x  % y   
+
+// // // === 'Odd'
+// // var remainderOperator2 = 17 % 4 
+
+// // if(remainderOperator1 === 0){
+// //   console.log('Remainder is Even')
+// // }else {
+// //   console.log('Remainder is Odd')
+// // }
+
+// // 6. Assignment Sign
+
+// // var myName = 'Adeyemi'
+// // var ismarried = false
+// // var myNumber = 20
+// // 7. Equality
+
+// // myName == 'Taiwo'  /// false
+// // var isNumber = myNumber === '20'  //strict Equality
+// // ismarried = true  //true
+
+// // if(ismarried == true){
+// //   console.log('Hurray!!! is Married')
+// // }else {
+// //   console.log('Please go get Marry')
+// // }
+
+
+// // 8.  Not Equality
+
+// var myName = 'Adeyemi'
+// var myNumber = 20
+
+
+// myName != 'Taiwo'  /// true
+// var isNumber = myNumber !== '20'   //strict Equality
+// ismarried = true  //true
+
+
+// console.log(isNumber)
+
+
+// const isMarried  = true 
+// console.log(!isMarried)
+// const dayOfTheWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thurday', 'Friday']
+
+// // const weekendDay = function(arr){
+// //   if(arr.indexOf('Sunday') === -1){
+// //     return "It's weekend"
+// //   }
+// // }
+
+// // console.log(weekendDay(dayOfTheWeek))
+// let myNname =  `ipsum dolor sit amet consectetur adipisicing elit. Dolore facilis eligendi perspiciatis suscipit, 
+// natus porro placeat necessitatibus inventore
+// ipsum dolor sit amet consectetur adipisicing elit. Dolore facilis eligendi perspiciatis suscipit, natus porro placeat necessitatibus inventore  ipsum dolor sit amet consectetur adipisicing elit. Dolore facilis eligendi perspiciatis suscipit, natus porro placeat necessitatibus inventore  ipsum dolor sit amet consectetur adipisicing elit. Dolore facilis eligendi perspiciatis suscipit, natus porro placeat necessitatibus inventore  ipsum dolor sit amet consectetur adipisicing elit. Dolore facilis eligendi perspiciatis suscipit, natus porro placeat necessitatibus inventore  ipsum dolor sit amet consectetur adipisicing elit. Dolore facilis eligendi perspiciatis suscipit, natus porro placeat necessitatibus inventore  ipsum dolor sit amet consectetur adipisicing elit. Dolore facilis eligendi perspiciatis suscipit, natus porro placeat necessitatibus inventore  ipsum dolor sit amet consectetur adipisicing elit. Dolore facilis eligendi perspiciatis suscipit, natus porro placeat necessitatibus inventore  ipsum dolor sit amet consectetur adipisicing elit. Dolore facilis eligendi perspiciatis suscipit, natus porro placeat necessitatibus inventore  ipsum dolor sit amet consectetur adipisicing elit. Dolore facilis eligendi perspiciatis suscipit, natus porro placeat necessitatibus inventore  ipsum dolor sit amet consectetur adipisicing elit. Dolore facilis eligendi perspiciatis suscipit, natus porro placeat necessitatibus inventore  ipsum dolor sit amet consectetur adipisicing elit. Dolore facilis eligendi perspiciatis suscipit, natus porro placeat necessitatibus inventore  ipsum dolor sit amet consectetur adipisicing elit. Dolore facilis eligendi perspiciatis suscipit, natus porro placeat necessitatibus inventore  ipsum dolor sit amet consectetur adipisicing elit. Dolore facilis eligendi perspiciatis suscipit, natus porro placeat necessitatibus inventore  ipsum dolor sit amet consectetur adipisicing elit. Dolore facilis eligendi perspiciatis suscipit, natus porro placeat necessitatibus inventore vv`
+
+ 
+// let intro = myNname
+
+// console.log(intro)
+
+
+
+// incrememt  by 1
+// var a = 4
+
+// a-= 21
+// console.log(a)
+
+// const letter = 'ipsum dolor sit amet consectetur adipisicing elit. Dolore \n facilis eligendi perspiciatis \n  suscipit, natus porro  \n placeat necessitatibus inventore '
+
+// console.log(letter)
+
+// let firstName = "Adeyemi";
+// let lastName = 'Ayodipe'
+
+// // let fullName = "My full name is" +  firstName + " Olushola " + lastName
+// let myName = 'Adeyemi'
+// let fullName = `My full name is ${firstName} ${lastName}`
+
+// var myStr = 'My name is ' + myName+ ' and I am well!'
+// console.log(myStr )
+
+// //Database
+// const database = [
+//   {name: 'Taiwo',
+//   city : 'Lexena',
+//   State:'Texas',
+//   Country: 'Usa'},
+
+//   {name: 'Adeyemi',
+//   city : 'Ikorodu',
+//   State:'Lagos',
+//   Country: 'Nigeria'}
+// ];
+// //Application
+// const bankApp =function(name){
+//   return 'Welcome ' + name + '. How are you doing today' }
+
+// console.log(bankApp('Taiwo'))
+
+//****** *****************/ Appending Variables to StringsPassed
+
+// // var num = 10
+
+// // num += 10
+
+// // console.log(num)
+
+// // var anAdjective = "awesome!";
+
+// // anAdjective += ' Brilliant'
+// // var ourStr = "freeCodeCamp is ";
+// // var anAdjective = "Awesome!"
+
+// // ourStr += anAdjective
+// // console.log(ourStr)
+
+
+// //*********************** */ Find the Length of a StringPassed
+
+// var stringLen = 'What is the length of this string'
+// console.log(stringLen.length)
+
+// //*************************/ Use Bracket Notation to Find the First Character in a String
+// // console.log(stringLen[3])
+
+// // Using the length position to find the last letter of a string
+
+
+// // Length = Character of the string
+// // position = Index of the character
+// // var x = 'Brilliant'
+// // console.log('last is :', x.length - 1)
+// // console.log(stringLen.length - 1)
+
+// // ******************Understand String ImmutabilityPassed
+
+// //String Immutability
+// // var myName = 'Adeyemi'
+
+
+// // console.log('1:', myName)
+
+// // myName[0]  = 'J'
+// // console.log('2:', myName)
+
+
+
+// // //Arr mutability
+// // const arr = ['a', 'b', 'c']
+
+// // console.log('1:', arr)
+
+// // arr[0] = 'n'
+
+// // console.log('2:', arr)
+
+
+
+// // ************************************Use Bracket Notation to Find the Nth Character in a StringPassed
+// // var myNoun = "dog";
+// // var myAdjective = "big";
+// // var myVerb = "ran";
+// // var myAdverb = "quickly";
+// // var wordBlanks = "The "+ myNoun + " looks so" + ' '+myAdjective +  " and "  + myVerb + " perfect "+  myAdverb + '.';
+
+// // console.log(wordBlanks)
+
+// // // Access Multi-Dimensional Arrays With Indexes
+// // var myArray = [
+// //     [1,2,3],
+// //     [4,5,6],
+// //     [7,8,9],
+    
+// //     [
+// //       [10,11,12], 
+// //       13, 
+// //       14
+// //     ]  
+
+// //   ];
+// // console.log(myArray[3][0][0]) 
+
+// // ********Manipulate Arrays With push()Passed
+
+// //push
+
+// const myArr = ['a', 'b', 'c', 'd', 'e']
+
+// console.log('Before', myArr)
+
+// myArr.push('a')
+// console.log('After', myArr)
+
+// //Pop
+
+// console.log(myArr.pop())
+
+// console.log('After', myArr)
+
+// //UnShift
+
+// myArr.unshift('g')
+// myArr.push('i')
+// console.log('After', myArr)
+// //unshift
+
+
+//Function
+
+// var guessName = B
+
+// "Hello Mr B, Welcome to my Party"
+
+// "Hello Mr Adeyemi Welcome to my Party"
+
+// "Hello Mr Adeyemi Welcome to my Party"
+
+// "Hello ............ Welcome to my Party"
+
+
+// function functionName(name, age, height){
+// var message = "Hello " + name + ", Welcome to my Party. I am " + age+ " years Old. I am " + height+ " inches tall"
+// return console.log(message)
+// };
+
+
+
+// functionName("Mr Adeyemi", 30, "5'6ft")
+// functionName('Kevin', 2, '26"')
+// // functionName('Taiwo')
+
+// Basic Function with argument
+
+// function reusableFunction(){
+//   var x = 10
+//   var y  = 5
+
+
+// var sum = x + y
+
+
+//   reusableFunction()
+
+// }
+
+// Function with argument
+
+// function reusableFunction(x, y){
+// var sum = x + y
+
+// console.log(sum)
+
+
+// }
+// console.log(reusableFunction(4, 5))
+
+
+
+// console.log("result :",result)
+// reusableFunction(3,4)
+// reusableFunction(12,14)
+// // reusableFunction(2,14)
+
+// function adeyemiLogin(){
+//   var name = 'Adeyemi'
+//   var userName = 'aadeyemi'
+
+//   var password = '123456'
+
+//   return 'aayodipe + 123455'
+// };
+
+// function taiwoLogin(){
+//   var name = 'taiwo'
+//   var userName = 'ataiwo'
+
+//   var password = '123456'
+//   return 'ataiwo + 123455'
+// };
+
+
+// function userLogin(name, username, password){
+//   return username + password
+// }
+
+// let adeyemi = userLogin('Adeyemi', 'aadeyemi', '123456')
+// // userLogin('taiwo', 'ataiwo', '123456')
+
+
+
+// function userX(){
+//   x = 3
+// console.log(x)
+// }
+
+// userX()
+
+
+// function fun2() {
+//   var output = "";
+//   if (typeof myGlobal != "undefined") {
+//     output += "myGlobal: " + myGlobal;
+//   }
+//   if (typeof oopsGlobal != "undefined") {
+//     output += " oopsGlobal: " + oopsGlobal;
+//   }
+//   console.log(output);
+// }
+
+// function timesFive(x) {
+// sum
+//   }
+  
+// console.log(timesFive(5))
+
+// function nextInLine(arr, item) {
+//   // Only change code below this line
+// arr.push(item)
+
+// console.log('RemovedITem', arr.shift() )//Remove the first Item pop() shift()
+
+// console.log('Remain', arr)
+//   return arr;
+//   // Only change code above this line
+  
+
+// }
+// function trueOrFalse() {
+//   // Only change code below this line
+
+//   var day = 4
+// if(day == 1){
+//  console.log('Today is Monday')
+// }else if(day ==2){
+//   console.log('Today is Tuesday')
+// }else if(day ==3){
+//   console.log('Today is Wednesday')
+// }else if(day ==4){
+//   console.log('Today is Thurday')
+// }else if(day ==5){
+//   console.log('Today is Friday')
+// }else if(day ==6){
+//   console.log('Today is Saturday')
+// }else if(day ==7){
+//   console.log('Today is Sunday')
+// }
+
+// switch(day){
+//   case 1: console.log('Today is Monday');
+//   break;
+//   case 2:console.log('Today is Tuesday')
+//   break;
+//   case 3:console.log('Today is Wednesday')
+//   break;
+// default: console.log('Day is not specified')
+// }
+
+// console.log(3 ===3)
+// console.log(3 ==='3')
+
+// console.log(3 ==3)
+
+// }
+// trueOrFalse()
+// console.log("3" < 7)
+// console.log('3'== 3)
+// // ==
+// // !=
+// // <
+// // >
+
+// function testLessOrEqual(val) {
+//   if (val <= 12) {  // Change this line
+//     return "Smaller Than or Equal to 12";
+//   }
+
+//   if (val <= 24) {  // Change this line
+//     return "Smaller Than or Equal to 24";
+//   }
+
+//   return "More Than 24";
+// }
+
+// console.log(testLessOrEqual(88));
+
+// 5 yrs old && male == soccer team
+
+// Person might either be a boy || a man  to attend  only boy's school'
+// function checkVal(val){
+
+// console.log('Hey I am here');
+
+//  if(val > 10 && val < 15 ){
+//      return console.log('Val is between 10 and 15')
+//   }else if(val > 20 || val < 30){  
+//     return console.log('Val is between 20and 30')
+  
+//   }else{  console.log('Val is not between 10 and 15 or 20 and 30' )};
+// return console.log('I am the last man' )
+
+// }
+
+// checkVal(15)
+// console.log('I am outside function')
+
+// function checkValinVal(val, x){
+// if(val > 5){
+//   if(5 < x){
+//     return console.log('The answer is true')
+//   }
+// }
+// console.log('the answer is false')
+// };
+
+// checkValinVal(6,7)
+
+// function createUserLogin(loginperson, user){
+
+//   if(loginperson === user){
+//     return console.log('User logged in')
+//   };
+//   return console.log('admin login in')
+// }
+
+// createUserLogin('admin','user')
+
+// var obj = {
+//   name: 'Fran',
+//   userName: 'aayodipe',
+//   address: 'Kansas',
+//   isMarried: true
+// };
+
+// console.log(`My name is ${obj.name}`)
+// console.log(`My name is ${obj.name}`)
+// console.log(`My name is ${obj.name}`)
+
+// obj.name='Frank'
+
+// console.log(`My name is ${obj.name}`)
+// obj.name='Frank'
+
+// console.log(`My name is ${obj.name}`)
+
+
+// console.log(`My name is ${obj.name}`)
+
+
+// console.log(`My name is ${obj.name}`)
+
+// ========================================
+
+// ///==============================
+// var someObj = {
+//   propName: "John"
+// };
+// function propPrefix(str) {
+//   var s = "prop";
+//   return s + str; // propName
+// }
+// var someProp = propPrefix("Name");
+
+
+// console.log(someObj[someProp]);  //John
+
+
+
+// var country = {
+//   Nigeria: 'Abuja',
+//   America: 'Washington DC',
+//   Britian: 'England'
+
+// };
+// country.America
+// let selectedCountry = function getCountryCapital(name){
+// return name 
+// };
+
+// console.log(country[selectedCountry('Nigeria')])
+// console.log(country[selectedCountry('Britian')])
+
+
+
+var dogs = {
+  Fido: "Mutt",  Hunter: "Doberman",  Snoopie: "Beagle"
+};
+
+var animal = 'Snoopie'
+
+dogs[animal]
+console.log(dogs['dobberman'])
+console.log(dogs[animal])
